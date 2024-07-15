ee6066298cc0354f4e38db705253ab0e38a50e98
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-07-15 13:56:44 -0500

    Add project files.

---
+++ AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoResenaApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://5211sc13-5192.brs.devtunnels.ms/";
+    }
+}
